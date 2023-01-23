085f669dd55b47de568e75791aa0264c57746fd5
retrok218 <retrok218@gmail.com>
2023-01-23 11:00:26 -0600

    Primer comit mesadeservicio

---
+++ app/Http/Controllers/MesaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MesaController extends Controller
+{
+    public function mesa_index_controller(){
+        return view('index');
+    }
+}
